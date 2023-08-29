a61d6a2256dc2d26fa5c7cefd2e9fcbb2c6e058e
Silvio Giancola <silvio.giancola@gmail.com>
2023-08-29 17:38:00 +0300

    minor

--- tools/infer.py
+++ tools/infer.py
@@ -1,32 +1,8 @@
-from snspotting.apis import init_spotter, inference_spotter
-
-
-
-
-
-
-
-# import logging
-# from datetime import datetime
-# import time
-# import numpy as np
+import os 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-# import torch
-# import mmengine
 from mmengine.config import Config, DictAction
-
-
-# import json
-
-
-# from snspotting.datasets import build_dataset, build_dataloader
-# from snspotting.models import build_model
-# from snspotting.loss import build_criterion
-# from snspotting.core import build_optimizer, build_scheduler
-
-# from snspotting.core.inference import infer_features, infer_game, infer_video
-# from snspotting.core.evaluation import evaluate_Spotting #testClassication, testSpotting
+from snspotting.apis import init_spotter, inference_spotter
 
 
 def parse_args():
@@ -35,14 +11,10 @@ def parse_args():
     parser.add_argument("config", metavar="FILE", type=str, help="path to config file")
     parser.add_argument("--input", type=str, help="path of game/features/video")
     parser.add_argument("--checkpoint", type=str, help="path of model checkpoint")
-    # parser.add_argument("--video", type=str, help="path of video")
-    # parser.add_argument("--features", type=str, help="path of features")
 
     parser.add_argument("--overwrite", action="store_true", help="whether to overwrite the results")
     parser.add_argument("--confidence_threshold", type=float, default=0.5, help="confidence threshold for results")
 
-    # not that important
-    parser.add_argument("--seed", type=int, default=42, help="random seed")
     parser.add_argument("--cfg-options", nargs="+", action=DictAction, help="override settings")
 
     # read args
@@ -70,7 +42,9 @@ def main():
     model = init_spotter(cfg, args.checkpoint, device='cuda:0')
 
     # test a single image and show the results
-    results = inference_spotter(cfg, model, input=args.input)
+    results = inference_spotter(cfg, model, 
+                            input=args.input,
+                            confidence_threshold=args.confidence_threshold)
 
     # print results
     print(f"Found {len(results['predictions'])} actions!")
