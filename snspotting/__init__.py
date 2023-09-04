c2f8b040d760672a117f616249eb8aa9d20ea112
SilvioGiancola <silvio.giancola@gmail.com>
2023-09-04 10:30:34 +0300

    minor

--- snspotting/__init__.py
+++ snspotting/__init__.py
@@ -1,9 +1,4 @@
-# from apis import *
-# from core import *
-# from datasets import *
-# from models import *
-
-__version_info__ = ('0', '0', '1')
+__version_info__ = ('0', '0', '2')
 __version__ = '.'.join(__version_info__)
 __authors__ = "Silvio Giancola"
 __authors_username__ = "giancos"
