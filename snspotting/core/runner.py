a2240c3728c664b1f5e54f6268a9198cfc8e8632
yassine <benzakouryassine2@gmail.com>
2023-12-19 15:57:22 +0100

    pylightning learnable pooling

--- snspotting/core/runner.py
+++ snspotting/core/runner.py
@@ -204,8 +204,7 @@ def timestamp_half(feat_half,model,BS):
         end_frame = BS*(b+1) if BS * \
             (b+1) < len(feat_half) else len(feat_half)
         feat = feat_half[start_frame:end_frame].cuda()
-        output = model(feat)
-        output.cpu().detach().numpy()
+        output = model(feat).cpu().detach().numpy()
         timestamp_long_half.append(output)
     return np.concatenate(timestamp_long_half)
 
